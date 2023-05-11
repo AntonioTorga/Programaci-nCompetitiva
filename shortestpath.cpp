a6db655307fb6cbbea0e0e479c6318c27b31d08f
AntonioTorga <antonio.torga@ug.uchile.cl>
2023-05-11 17:26:31 -0400

    avance un poco not done eso si

--- shortestpath.cpp
+++ shortestpath.cpp
@@ -5,12 +5,13 @@ long long INF = 1e18;
 
 int main(){
     while(true){
-        int N, M, S, D;
+        int N, M;
         cin >> N >> M;
-        if (N==0 && M==0) return 0;
+        if (N==0 && M==0) {
+            return 0;
+            }
+        int S, D;
         cin >> S >> D;
-        S--;
-        D--;
 
         vector<vector<pair<int, long long>>> adjS(N);
         vector<vector<pair<int, long long>>> adjD(N);
@@ -19,15 +20,14 @@ int main(){
         while (M--){
             int U,V,P;
             cin >> U >> V >> P;
-            U--;V--;
             adjS[U].push_back({V,P});
             adjD[V].push_back({D,P});
-            adjFin[U].push_back({V,P});
-
         }
+
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
         vector <long long> distS (N,INF);
         vector <long long> distD (N,INF);
+        vector <long long> distFin (N,INF);
 
         pq.push({S,0});
         distS[0]=0;
@@ -61,10 +61,22 @@ int main(){
             auto itr = adjFin[i].begin();
             for (int j = 0; j < adjS[i].size(); i++){
                 auto [node, peso] = adjS[i][j];
-                if (distS[i] + peso + distD[node] == distS[D]){ adjFin[i].erase(itr+j); }
-
+                if (distS[i] + peso + distD[node] != distS[D]){ adjFin[i].push_back(make_pair(node,peso)); }
             }
         }
 
+        pq.push({S,0});
+        distFin[0]=0;
+        while(!pq.empty()){
+            auto [peso_camino, u] = pq.top(); pq.pop();
+            if (peso_camino != distFin[u]) continue;
+            for (auto [v,w]: adjFin[u]){
+                if(peso_camino+w<distFin[v]){
+                    distFin[v] = peso_camino + w;
+                    pq.push({distFin[v],v});
+                }
+            }
+        }
+        cout << distFin[D] << endl;
     }
 }
